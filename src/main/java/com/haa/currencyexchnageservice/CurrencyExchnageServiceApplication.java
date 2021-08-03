c24530d155c6fdf385c340b668b8b3fbd957c147
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-08-03 07:43:40 +0530

    first commit

---
+++ src/main/java/com/haa/currencyexchnageservice/CurrencyExchnageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haa.currencyexchnageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchnageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchnageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haa/currencyexchnageservice/CurrencyExchnageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haa.currencyexchnageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchnageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

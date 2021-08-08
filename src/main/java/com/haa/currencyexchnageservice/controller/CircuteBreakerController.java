47a16f9a728e3b624443c824f228753aca0693a5
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-08-08 23:36:03 +0530

    circuite break down if request fails

--- src/main/java/com/haa/currencyexchnageservice/controller/CircuteBreakerController.java
+++ src/main/java/com/haa/currencyexchnageservice/controller/CircuteBreakerController.java
@@ -1,14 +1,27 @@
 package com.haa.currencyexchnageservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuteBreakerController {
 
+    private Logger logger = LoggerFactory.getLogger(CircuteBreakerController.class);
+
     @GetMapping("/sample-api")
+    @Retry(name = "default")
     public String sampleApi() {
-        return "Sample API";
+        logger.info("Sample API call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8001/sample-api-dummy",
+                String.class);
+
+        return forEntity.getBody();
     }
 
 }

fe7ca0f975ebadd1a7120754a9035de4c86b3108
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-08-18 07:05:19 +0530

    configuration changes to run on kubernetes

--- src/main/java/com/haa/currencyexchnageservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/haa/currencyexchnageservice/controller/CurrencyExchangeController.java
@@ -6,6 +6,8 @@ import com.haa.currencyexchnageservice.bean.CurrencyExchange;
 import com.haa.currencyexchnageservice.exception.CurrencyNotFoundException;
 import com.haa.currencyexchnageservice.repository.CurrencyExchangeRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository repository;
@@ -24,6 +28,8 @@ public class CurrencyExchangeController {
     @GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange fetchCurrencyExchange(@PathVariable String from, @PathVariable String to) {
 
+        logger.info("Currency exchange called from {} to {}", from, to);
+
         // CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to,
         // BigDecimal.valueOf(50));
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
@@ -31,7 +37,9 @@ public class CurrencyExchangeController {
             throw new CurrencyNotFoundException("Unable to Find data for " + from + " to " + to);
 
         String port = environment.getProperty("local.server.port");
-        currencyExchange.setEnvironment(port);
+        String host = environment.getProperty("HOSTNAME");
+
+        currencyExchange.setEnvironment(port + " " + host);
 
         return currencyExchange;
     }
